353b1e05ba8ff0912e773c8829f9e0f3afc9dec3
livbig <livbigovich@gmail.com>
2020-04-09 19:50:10 +0300

    Test creation of 4 home screen buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
+//INITIALIZING THE APP
+
+void main() => runApp(APP());
+
 class APP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HOME(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.orange,
+      ),
+      home: Home(),
     );
   }
 }
 
-class HOME extends StatelessWidget {
+//CREATING BUTTONS FOR HOME SCREEN
+
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
