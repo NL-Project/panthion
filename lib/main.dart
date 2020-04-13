42196416dce0d7efdb21e42b7f6581d9ffbdd4c9
illfalcon <kostasminch@gmail.com>
2020-04-13 12:29:55 +0300

    added pdftron and refactored button a bit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pdftron_flutter/pdftron_flutter.dart';
 
 const String home = "Home";
 
@@ -21,42 +22,70 @@ class Panthion extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String version;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      PdftronFlutter.initialize(
+          "Insert commercial license key here after purchase");
+      version = await PdftronFlutter.version;
+    } catch (e) {
+      version = 'Failed to get platform version.';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          HomeButton(
-            "Button 1",
-            () {
-              Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text("Button 1 has been pressed")));
-            },
-          ),
-          HomeButton(
-            "Button 2",
-            () {
-              Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text("Button 2 has been pressed")));
-            },
-          ),
-          HomeButton(
-            "Button 3",
-            () {
-              Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text("Button 3 has been pressed")));
-            },
-          ),
-          HomeButton(
-            "Button 4",
-            () {
-              Scaffold.of(context).showSnackBar(
-                  SnackBar(content: Text("Button 4 has been pressed")));
-            },
-          ),
-        ],
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 10.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            HomeButton(
+              "Open Document",
+                  // TODO: doesn't work without internet
+                  () => PdftronFlutter.openDocument(
+                  "https://file-examples.com/wp-content/uploads/2017/02/file-sample_1MB.docx"),
+            ),
+            HomeButton(
+              "Button 2",
+                  () {
+                Scaffold.of(context).showSnackBar(
+                    SnackBar(content: Text("Button 2 has been pressed")));
+              },
+            ),
+            HomeButton(
+              "Button 3",
+                  () {
+                Scaffold.of(context).showSnackBar(
+                    SnackBar(content: Text("Button 3 has been pressed")));
+              },
+            ),
+            HomeButton(
+              "Button 4",
+                  () {
+                Scaffold.of(context).showSnackBar(
+                    SnackBar(content: Text("Button 4 has been pressed")));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -69,19 +98,22 @@ class HomeButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
-      color: Colors.transparent,
-      textColor: Colors.red,
-      padding: const EdgeInsets.all(20),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(35),
-        side: BorderSide(color: Colors.red, width: 3),
+    return SizedBox(
+      width: double.infinity,
+      child: FlatButton(
+        color: Colors.transparent,
+        textColor: Colors.red,
+        padding: const EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(35),
+          side: BorderSide(color: Colors.red, width: 3),
+        ),
+        child: Text(
+          _text,
+          style: TextStyle(fontSize: 40),
+        ),
+        onPressed: _onPressed,
       ),
-      child: Text(
-        _text,
-        style: TextStyle(fontSize: 50),
-      ),
-      onPressed: _onPressed,
     );
   }
 }
