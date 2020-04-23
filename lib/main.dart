f37bdb6e8071d62a3cd6e49fb48a714d831a3bb2
livbig <livbigovich@gmail.com>
2020-04-23 22:03:26 +0300

    Update on theme + First attempt on calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:pdftron_flutter/pdftron_flutter.dart';
+
+final List<String> duties= <String>['23.05.2020', '24.05.2020', '25.05.2020'];
 
 const String home = "Home";
 
@@ -12,17 +15,32 @@ class Panthion extends StatelessWidget {
     return MaterialApp(
       title: 'Panthion',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        brightness: Brightness.light,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.white,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.indigo,
+          shape: StadiumBorder(),
+          textTheme: ButtonTextTheme.accent),
+        appBarTheme: AppBarTheme(
+          color: Colors.indigo),
+        primaryTextTheme: TextTheme(
+          title: TextStyle(color: Colors.white, fontSize: 26),
+          headline: TextStyle(color: Colors.white, fontSize: 40))
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome, USER'),
-          ),
-          backgroundColor: Colors.teal[100],
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(150),
+          child: AppBar(
+          centerTitle: true,
+          flexibleSpace: FlexibleSpaceBar(title: Text("Next duty is on ${duties[0]}!"), centerTitle: true,),
+          title: Text("Welcome, User!"))),
           body: Home()),
     );
   }
 }
+
+//---------HOME SCREEN HERE-------------------
 
 class Home extends StatefulWidget {
   @override
@@ -54,7 +72,7 @@ class _HomeState extends State<Home> {
     return Center(
       child: Padding(
         padding: EdgeInsets.symmetric(
-          horizontal: 10.0,
+          horizontal: 40.0,
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -92,15 +110,31 @@ class _HomeState extends State<Home> {
               ),
             ),
             HomeButton(
-              "Button 4",
+              "Calendar",
               () => Navigator.of(context).push(
                 PageTransition(
                   type: PageTransitionType.rightToLeft,
                   child: Scaffold(
                     appBar: AppBar(
-                      title: Text("Empty Screen"),
+                      title: Text("Calendar"),
                     ),
-                  ),
+                  body: ListView.separated(
+                  padding: const EdgeInsets.all(20),
+                  itemCount: duties.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        elevation: 6,
+                        color: Theme.of(context).primaryColor,
+                        padding: const EdgeInsets.all(20),
+                          child: Text(
+                            'Next duty is on ${duties[index]}',
+                            style: TextStyle(fontSize: 24, color: Colors.white)),
+                        onPressed: () {},)
+                    );},
+                    separatorBuilder: (BuildContext context, int index) => Divider())
+                  )
                 ),
               ),
             ),
@@ -122,15 +156,9 @@ class HomeButton extends StatelessWidget {
       width: double.infinity,
       child: RaisedButton(
         elevation: 6,
-        color: Colors.deepOrangeAccent,
-        textColor: Colors.white,
-        padding: const EdgeInsets.all(25),
-        shape: StadiumBorder(
-          ),
-        child: Text(
-          _text,
-          style: TextStyle(fontSize: 30),
-        ),
+        color: Theme.of(context).primaryColor,
+        padding: const EdgeInsets.all(20),
+        child: Text(_text, style: TextStyle(fontSize: 24),),
         onPressed: _onPressed,
       ),
     );
