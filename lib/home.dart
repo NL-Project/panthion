95d65529fe49b516b346927c41fc951933f6cd47
illfalcon <kostasminch@gmail.com>
2020-04-26 14:50:44 +0300

    separated by files

---
+++ lib/home.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:pdftron_flutter/pdftron_flutter.dart';
+
+import 'home_button.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String version;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      PdftronFlutter.initialize(
+          "Insert commercial license key here after purchase");
+      version = await PdftronFlutter.version;
+    } catch (e) {
+      version = 'Failed to get platform version.';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var mainWidget = Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            HomeButton(
+              "Instructions",
+              // TODO: doesn't work without internet
+              () => PdftronFlutter.openDocument(
+                  "https://file-examples.com/wp-content/uploads/2017/02/file-sample_1MB.docx"),
+            ),
+            HomeButton(
+              "Button 2",
+              () => Navigator.of(context).push(
+                PageTransition(
+                  type: PageTransitionType.rightToLeft,
+                  child: Scaffold(
+                    appBar: AppBar(
+                      title: Text("Empty Screen"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            HomeButton(
+              "Button 3",
+              () => Navigator.of(context).push(
+                PageTransition(
+                  type: PageTransitionType.rightToLeft,
+                  child: Scaffold(
+                    appBar: AppBar(
+                      title: Text("Empty Screen"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            HomeButton(
+              "Button 4",
+              () => Navigator.of(context).push(
+                PageTransition(
+                  type: PageTransitionType.rightToLeft,
+                  child: Scaffold(
+                    appBar: AppBar(
+                      title: Text("Empty Screen"),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello, User!"),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) => orientation == Orientation.portrait
+            ? mainWidget
+            : SingleChildScrollView(
+                child: mainWidget,
+              ),
+      ),
+    );
+  }
+}
