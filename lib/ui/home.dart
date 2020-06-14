106a39519925003dc7443cb72f732033c87af9e0
illfalcon <kostasminch@gmail.com>
2020-06-14 22:43:53 +0300

    added actual instructions

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:panthion/ui/calendar.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pdftron_flutter/pdftron_flutter.dart';
 import 'log_in_button_2.dart';
 import 'notes.dart';
+import 'package:path/path.dart';
 import 'home_button.dart';
 
 final List<String> duties = <String>['23.05.2020', '24.05.2020', '25.05.2020'];
@@ -35,79 +40,85 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-     var mainWidget = GridView.count(
-       primary: false,
-          padding: const EdgeInsets.all(0),
-          crossAxisSpacing: 0,
-          mainAxisSpacing: 0,
-          crossAxisCount: 2,
-          children: <Widget>[
-            HomeButton(
-              Colors.red,
-              "assets/help.png",
-              "Wat te doen?",
-              // TODO: doesn't work without internet
-                  () => PdftronFlutter.openDocument(
-                  "https://file-examples.com/wp-content/uploads/2017/02/file-sample_1MB.docx"),
-            ),
-            HomeButton(
-              Colors.green,
-              "assets/list.png",
-              "Regeling",
-                  () => Navigator.of(context).push(
-                PageTransition(
-                  type: PageTransitionType.rightToLeft,
-                  child: Log(),
-                ),
+    var mainWidget = GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(0),
+        crossAxisSpacing: 0,
+        mainAxisSpacing: 0,
+        crossAxisCount: 2,
+        children: <Widget>[
+          HomeButton(
+            Colors.red,
+            "assets/help.png",
+            "Wat te doen?",
+            // TODO: doesn't work without internet
+            () async {
+              Directory directory = await getApplicationDocumentsDirectory();
+              var dbPath = join(directory.path, "info.docx");
+              ByteData data = await rootBundle.load("assets/informatie.docx");
+              List<int> bytes = data.buffer
+                  .asUint8List(data.offsetInBytes, data.lengthInBytes);
+              await File(dbPath).writeAsBytes(bytes);
+              PdftronFlutter.openDocument(dbPath);
+            },
+          ),
+          HomeButton(
+            Colors.green,
+            "assets/list.png",
+            "Regeling",
+            () => Navigator.of(context).push(
+              PageTransition(
+                type: PageTransitionType.rightToLeft,
+                child: Log(),
               ),
             ),
-            HomeButton(
-              Colors.yellow,
-              "assets/light.png",
-              "Tips",
-                  () => Navigator.of(context).push(
-                PageTransition(
-                  type: PageTransitionType.rightToLeft,
-                  child: Notes(),
-                ),
+          ),
+          HomeButton(
+            Colors.yellow,
+            "assets/light.png",
+            "Tips",
+            () => Navigator.of(context).push(
+              PageTransition(
+                type: PageTransitionType.rightToLeft,
+                child: Notes(),
               ),
             ),
-            HomeButton(
-              Colors.blue,
-              "assets/schedule.png",
-               "Rooster",
-                  () => Navigator.of(context).push(
-                PageTransition(
-                  type: PageTransitionType.rightToLeft,
-                  child: Calendar(),
-                ),
+          ),
+          HomeButton(
+            Colors.blue,
+            "assets/schedule.png",
+            "Rooster",
+            () => Navigator.of(context).push(
+              PageTransition(
+                type: PageTransitionType.rightToLeft,
+                child: Calendar(),
               ),
             ),
-          ]
-     );
+          ),
+        ]);
 
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       appBar: PreferredSize(
           preferredSize: Size.fromHeight(150),
           child: AppBar(
-            elevation: 0,
+              elevation: 0,
               centerTitle: true,
-              flexibleSpace: Padding(padding: EdgeInsets.all(50), child: Image.asset('assets/enver.png'))
-             )
-      ),
+              flexibleSpace: Padding(
+                  padding: EdgeInsets.all(50),
+                  child: Image.asset('assets/enver.png')))),
       body: OrientationBuilder(
         builder: (context, orientation) => orientation == Orientation.portrait
             ? mainWidget
             : SingleChildScrollView(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                mainWidget,
-                Text('Hello', style: TextStyle(color: Colors.white))
-              ],
-            ),
-        ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    mainWidget,
+                    Text('Hello', style: TextStyle(color: Colors.white))
+                  ],
+                ),
+              ),
       ),
     );
   }
